25c488de600bc07e6955d7d6a334e8059e1aaaa7
Guillaume Bartolini <guillaumebartolini@gmail.com>
2012-03-19 12:42:56 +1100

    frst commit of the starting template

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Dynamic loading configuration
+Modernizr.load([
+    {
+        load: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
+        complete: function () {
+            if (!window.jQuery) {
+                Modernizr.load('js/jquery-1.7.1.min.js');
+            }
+            
+            Modernizr.load([
+            	'js/custom.js?v=01'
+            ]);
+            
+        }
+    },{
+        load : 'ielt7!/js/DD_belatedPNG_0.0.8a-min.js',
+        callback: function() {
+            $(function() {
+                DD_belatedPNG.fix('img, .png_bg');
+            }); 
+        }
+    }
+]);
