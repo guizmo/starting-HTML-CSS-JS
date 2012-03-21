1548bdc78c6fd7e796fe994b5126b79d0f463fb2
Guillaume Bartolini <guillaumebartolini@gmail.com>
2012-03-21 19:38:54 +1100

    fix IE bug placeholder input

--- js/main.js
+++ js/main.js
@@ -12,29 +12,6 @@ Modernizr.load([
             	'js/custom.js?v=01'
             ]);
             
-            if(!Modernizr.input.placeholder){
-            	$('[placeholder]').focus(function() {
-            	  var input = $(this);
-            	  if (input.val() == input.attr('placeholder')) {
-            		input.val('');
-            		input.removeClass('placeholder');
-            	  }
-            	}).blur(function() {
-            	  var input = $(this);
-            	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
-            		input.addClass('placeholder');
-            		input.val(input.attr('placeholder'));
-            	  }
-            	}).blur();
-            	$('[placeholder]').parents('form').submit(function() {
-            	  $(this).find('[placeholder]').each(function() {
-            		var input = $(this);
-            		if (input.val() == input.attr('placeholder')) {
-            		  input.val('');
-            		}
-            	  })
-            	});
-            }
         }
     },{
         load : 'ielt7!/js/DD_belatedPNG_0.0.8a-min.js',
